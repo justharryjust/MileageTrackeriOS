db4c661688476b62c91de10d2148b7e86e1b5397
harry.just <harry.just@live.com>
2026-04-21 19:03:53 +1200

    Initial Commit

---
+++ MileageTrackeriOSTests/MileageTrackeriOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MileageTrackeriOSTests.swift
+//  MileageTrackeriOSTests
+//
+//  Created by Harry Just on 21/04/2026.
+//
+
+import Testing
+@testable import MileageTrackeriOS
+
+struct MileageTrackeriOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
